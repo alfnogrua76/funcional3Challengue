fc5124471b2b6ed33b48e5e59f1da2a990ded887
Alfredo Noguera <alfnogrua@gmail.com>
2025-03-07 23:37:40 -0500

    Challengue4

--- domain/usecase/src/main/java/co/com/bancolombia/usecase/account/AccountUseCase.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/account/AccountUseCase.java
@@ -14,43 +14,35 @@ public class AccountUseCase {
     private final AccountRepository repository;
 
     public Mono<Account> register(String name, String statusId){
+        //como se pueden ir concatenando pasos de funciones lambdas para que queden dentro del
+        //mismo scope
+
+        var n1 = Mono.just(1);
+        var n2 = Mono.just(2);
+        var n3 = Mono.just(3);
+        var n4 = Mono.just(4);
+        n1
+        .flatMap(m1 -> n2
+                .flatMap(m2 -> n2
+                        .flatMap(m3 -> n4
+                                .map(m4 -> m1 + m2 + m3 + m4  ))))
+                .subscribe(value -> {
+                    System.out.println("***************");
+                    System.out.println("***************");
+                    System.out.println(value);
+                    System.out.println("***************");
+                });
+
         return getStatus(name)
                 //Tambien se puede hacer pos Asociatividad
                 .flatMap(status -> Mono.zip(//par hacer las 2 primeras validaciones en paralelo
                         legalValidation(name),
                         disponibilityValidation()
                 )
-                /*
-                .map(TupleUtils.function((n1,n2)->{
-                    return n1.toString();
-                }))
-                *///quito la forma de sacar las variables de la tupla  y paso el cierre del ultimo parentesis
-                // al de abajo linea 22 quito el ultimo ) y lo coloque en la 29 el ultimo )
                 .map(sta -> generateAccount(name, status)))
                 .flatMap(this::finalValidation)
                 .flatMap(this::saveAccount);
-         /*
-        return Mono.zip(//par hacer las 2 primeras validaciones en paralelo
-                        legalValidation(name),
-                        disponibilityValidation()
-                )
-                /*      Manera practica con utilidad reactor de usar la tupla
-                        .map(TupleUtils.function((n1,n2)->{
-                            return n1.toString();
-                        }))
 
-                 */
-                //        legalValidation(name)
-                //        .flatMap(legal -> disponibilityValidation())
-                /*       .map(tuple ->{//una forma de trabajar la tupla
-                           String t1 = tuple.getT1();
-                           Integer t2 = tuple.getT2();
-                           return "ALGO";
-                       })*/
-
-                //map(status -> generateAccount(name, status)).
-//                .flatMap(this::finalValidation)
-//                .flatMap(this::saveAccount);
     }
 
     private Account generateAccount (String name, String status){
